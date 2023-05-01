702e4aebfe98a3187cb3e1b37a0403fe1196f3c9
Iván <ivanlitt8@gmail.com>
2023-05-01 17:10:01 -0300

    modificacion de gitignore

--- client/script.js
+++ client/script.js
@@ -79,6 +79,33 @@ const handleSubmit = async (e) => {
 
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+    console.log(parsedData);
+
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something went wrong"
+    alert(err)
+  }
 }
 
 form.addEventListener('submit', handleSubmit)
@@ -87,30 +114,3 @@ form.addEventListener('keyup', (e) => {
     handleSubmit(e)
   }
 })
-
-
-// const response = await fetch('https://codex-im0y.onrender.com/', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
-//       prompt: data.get('prompt')
-//     })
-//   })
-
-//   clearInterval(loadInterval)
-//   messageDiv.innerHTML = " "
-
-//   if (response.ok) {
-//     const data = await response.json();
-//     const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-//     typeText(messageDiv, parsedData)
-//   } else {
-//     const err = await response.text()
-
-//     messageDiv.innerHTML = "Something went wrong"
-//     alert(err)
-//   }
-// }
